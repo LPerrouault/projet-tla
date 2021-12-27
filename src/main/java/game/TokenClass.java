54b7fe916ef36be5697fdb7cde4cb677329f1f88
Manon <mmanon37270@gmail.com>
2021-12-27 16:58:30 +0100

    analyse syntaxique début

--- src/main/java/game/TokenClass.java
+++ src/main/java/game/TokenClass.java
@@ -1,7 +1,7 @@
 package game;
 
 public enum TokenClass {
-    intVal, rightPar, comma,
+    intVal, rightPar, comma,string,
     /*Fonctions:*/ playerStart, dioStart, dioActions, dioMove, dioPrepare, 
     dioAttaque, playerWait, setWalls,
     /*Types d'attaque*/ couteau, rouleau
