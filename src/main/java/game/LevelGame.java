e8e645b6000d2d3ab12728659a62a5f581e9e9fb
Inès FEUGIER <inesfeugier@gmail.com>
2021-12-29 19:09:34 +0100

    Suppression des fonctions en trop dans LevelGame

--- src/main/java/game/LevelGame.java
+++ src/main/java/game/LevelGame.java
@@ -2,7 +2,6 @@ package game;
 
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.util.ArrayList;
 import java.util.Scanner;
 
 public class LevelGame{
@@ -47,76 +46,4 @@ public class LevelGame{
         }
         return line;
     }
-    /*
-    public Edio getEdioLevel1() {
-        ArrayList<Integer> sequenceMouvements = new ArrayList<Integer>();
-        sequenceMouvements.add(4);
-        sequenceMouvements.add(6);
-        sequenceMouvements.add(1);
-        sequenceMouvements.add(8);
-        sequenceMouvements.add(11);
-        Edio edio = new Edio(12,
-                new EdioAction[]{
-                    EdioAction.MOVE,
-                    EdioAction.PREPARE_COUTEAU,
-                    EdioAction.ATTACK_COUTEAU,
-                    EdioAction.MOVE,
-                    EdioAction.PREPARE_ROULEAU,
-                    EdioAction.ATTACK_ROULEAU,
-                    EdioAction.MOVE,
-                    EdioAction.PREPARE_COUTEAU,
-                    EdioAction.ATTACK_COUTEAU,
-                    EdioAction.MOVE,
-                    EdioAction.PREPARE_ROULEAU,
-                    EdioAction.ATTACK_ROULEAU,
-                    EdioAction.MOVE,
-                    EdioAction.PREPARE_COUTEAU,
-                    EdioAction.ATTACK_COUTEAU
-                },
-                sequenceMouvements);
-        return edio;
-    }
-
-    public Lemonde getLeMondeLevel1() {
-        int[][] tabCases = {
-                {1, 2},
-                {8, 8}
-        };
-        Lemonde lemonde = new Lemonde(tabCases);
-        return lemonde;
-    }
-    
-    public Lemonde getLeMondeLevel2() {
-        int[][] tabCases = {
-                {1, 2},
-                {8, 8}
-        };
-        Lemonde lemonde = new Lemonde(tabCases);
-        return lemonde;
-    }
-
-    public Edio getEdioLevel2() {
-        ArrayList<Integer> sequenceMouvements = new ArrayList<Integer>();
-        sequenceMouvements.add(4);
-        sequenceMouvements.add(2);
-        sequenceMouvements.add(6);
-        sequenceMouvements.add(8);
-        Edio edio = new Edio(11,
-                new EdioAction[]{
-                    EdioAction.MOVE,
-                    EdioAction.PREPARE_COUTEAU,
-                    EdioAction.ATTACK_COUTEAU,
-                    EdioAction.MOVE,
-                    EdioAction.PREPARE_ROULEAU,
-                    EdioAction.ATTACK_ROULEAU,
-                    EdioAction.MOVE,
-                    EdioAction.PREPARE_COUTEAU,
-                    EdioAction.ATTACK_COUTEAU,
-                    EdioAction.MOVE,
-                    EdioAction.PREPARE_ROULEAU,
-                    EdioAction.ATTACK_ROULEAU
-                },
-                sequenceMouvements);
-        return edio;
-    }*/
 }
